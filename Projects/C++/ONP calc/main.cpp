5f2c2ba72f461d15ad58b9d7ea5369bea1cd8431
Maksymilian <92531282+Maksymilian21Wnuk@users.noreply.github.com>
2022-06-13 15:48:32 +0200

    Create main.cpp

---
+++ Projects/C++/ONP calc/main.cpp
@@ -0,0 +1,20 @@
+#include "headers.hpp"
+
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <sstream>
+
+using namespace std;
+
+
+
+int main(){
+    Eval r;
+    Eval r2;
+    r.run();
+
+
+
+    return 0;
+}
