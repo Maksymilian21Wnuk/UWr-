3e6fb764c3c90c3b52a1bfa80afe43fe70954672
meks <maksymilian21wnuk@gmail.com>
2024-03-17 20:02:28 +0100

    add l3 digital

--- digital_logic/l2/toplevel.sv
+++ digital_logic/l2/toplevel.sv
@@ -1,5 +1,5 @@
 
-module my_module(output o, input [3:0] i);
+module toplevel(output o, input [3:0] i);
     // solved using k-map
     // some temporal variables to get readability
     logic l1 = !i[0] && i[1] && i[3];
@@ -8,5 +8,11 @@ module my_module(output o, input [3:0] i);
     logic l4 = i[0] && !i[1] && i[2];
     logic l5 = !i[0] && i[1] && i[2];
     logic l6 = !i[1] && i[2] && i[3];
-    assign o = l1 || l2 || l3 || l4 || l5 || l6;
+    logic l7 = i[0] && i[1] && !i[2];
+    logic l8 = i[1] && i[2] && !i[3];
+    logic l9 = i[0] && i[2] && !i[3];
+    logic l10 = i[1] && !i[2] && i[3];
+    logic l11 = !i[0] && i[2] && i[3];
+    logic l12 = i[0] && !i[1] && i[3];
+    assign o = l1 || l2 || l3 || l4 || l5 || l6 || l7 || l8 || l9 || l10 || l11 || l12;
 endmodule
